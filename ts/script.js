c8e5e06574db9ec5efb1ba2a70680cdd9eeb7dee
MD Sanju <mdsanju387@gmail.com>
2022-04-25 16:22:33 +0600

    typescript map

--- ts/script.js
+++ ts/script.js
@@ -1,5 +1,5 @@
-console.log("Penis, Vagina");
+console.log("Hello World");
 var arr = [12, 25, 45, 56];
-var arr2 = ["Go", 'Munia', "Sanju", "Canada", 'Python'];
+var arr2 = ["Go", "Munia", "Sanju", "Canada", "Python"];
 var mapping = arr.map(function (e) { return e * 2; });
 console.log(mapping);
--- ts/script.ts
+++ ts/script.ts
@@ -1,9 +1,8 @@
-console.log("Penis, Vagina");
+console.log("Hello World");
 
 const arr: number[] = [12, 25, 45, 56];
-const arr2: string[] = ["Go", 'Munia', "Sanju", "Canada", 'Python'];
+const arr2: string[] = ["Go", "Munia", "Sanju", "Canada", "Python"];
 
-const mapping: number[] = arr.map(e => e * 2);
+const mapping: number[] = arr.map((e) => e * 2);
 
 console.log(mapping);
-
