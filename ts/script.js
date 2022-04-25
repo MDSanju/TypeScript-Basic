dbbc981a7e55ec2fb76915367367db870e29cf1e
MD Sanju <mdsanju387@gmail.com>
2022-04-25 16:16:29 +0600

    basic TypeScript da-1

---
+++ ts/script.js
@@ -0,0 +1,5 @@
+console.log("Penis, Vagina");
+var arr = [12, 25, 45, 56];
+var arr2 = ["Go", 'Munia', "Sanju", "Canada", 'Python'];
+var mapping = arr.map(function (e) { return e * 2; });
+console.log(mapping);
---
+++ ts/script.ts
@@ -0,0 +1,9 @@
+console.log("Penis, Vagina");
+
+const arr: number[] = [12, 25, 45, 56];
+const arr2: string[] = ["Go", 'Munia', "Sanju", "Canada", 'Python'];
+
+const mapping: number[] = arr.map(e => e * 2);
+
+console.log(mapping);
+
